99e3dc3f8d4b4ba11f4341e6e7274ff3f6111b3a
smartvnm <laurex@mail.com>
2024-03-04 17:00:53 +0200

    deleted:    tests/

--- tests/drv.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-cmd = ["wget", "http://docs.google.com/uc?export=open&id=1GgOgE_S_r4yvRyo0X9VVdEDfOMdWoux0","-O","somes.mp3"]
-result = subprocess.run(cmd)
--- tests/fixaudio.py
+++
@@ -1,8 +0,0 @@
-import json
-with open("audiolibrary.json","r") as f:
-  t = json.load(f)
-  t = json.dumps(t).replace("export=download","export=open")
-with open("audiolibrary.json","w") as ff:
-  tt =json.loads(t)
-  json.dump(tt,ff)
-    
